42827a35a65177982a2831f2664aeeca05051c4a
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-08-25 14:37:53 +0530

    var,let,const,comments are added

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello world-1");
+//console.log("Hello world-1");
+//console.log("Hello world-2");
+//  console.log("Hello world-3");
+
+
+//var age = 10;
+//var age = 20;
+
+
+
+
+
+//can be reassigned and cant be redeclared//
+//let age = 10;
+//age = 20;
+//console.log(age);
+
+
+
+//cant be reassigned and redeclared//
+const age = 10;
+console.log(age);
+
+
+ 
