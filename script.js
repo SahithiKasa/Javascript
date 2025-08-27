577faf1158d329ec8083ef46c8ca7d7586e80122
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-08-27 12:29:04 +0530

    Datatypes

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-//console.log("Hello world-1");
-//console.log("Hello world-2");
-//  console.log("Hello world-3");
+//console.log("Hello world - 1");
+//console.log("Hello world - 2");
+//console.log("Hello world - 3");
+
 
 
 //var age = 10;
-//var age = 20;
-
-
-
-
-
-//can be reassigned and cant be redeclared//
-//let age = 10;
-//age = 20;
+//    age = 20;
 //console.log(age);
 
 
+// re-assign and re-declare
+// var age = 10;
+// var age = 20;
 
-//cant be reassigned and redeclared//
+
+//re-assign cannot do re-declare
+// let age = 10;
+//     age = 20;
+
+//cannot re-assign cannot do re-declare
 const age = 10;
 console.log(age);
-
-
- 
