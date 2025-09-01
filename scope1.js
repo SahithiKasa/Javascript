bd6d25a2403408ab16e8dd65e78ffd4153dfe9b9
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-09-01 14:56:28 +0530

    scope

---
+++ scope1.js
@@ -0,0 +1,36 @@
+//var greenColor = "Green";
+
+//localblock-1
+//{
+//     let blueColor = "blue";
+//  console.log("Localblock1" , greenColor);
+//     console.log("Blue color is" , blueColor);
+//     console.log("localblok2 is" ,pinkColor);
+ //}
+
+
+//localblock-2
+//{
+//     let pinkColor = "pink"
+//     console.log("Localblock2" , greenColor);
+//      console.log("Localblock1" ,blue);
+//}
+
+
+
+
+
+var name="madan";
+function greet(){
+    var message="Hello" + "madan";
+    console.log(message);
+    
+    function expression(){
+        var emoji="😊";
+        console.log(message + " " + emoji);
+    }
+    expression();
+    }
+    greet();
+
+
