808170d773524c2c5def69cf9a5ecebf60f8f01c
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-09-02 23:04:28 +0530

    debug2

--- debugging-codetech.js
+++ debugging-codetech.js
@@ -1,26 +1,52 @@
-let foodItemName= "Pizza";
-console.log(foodItemName);
+//let foodItemName= "Pizza";
+//console.log(foodItemName);
 
 
 
 
-function foodApp(item){
-    console.log("FoodApplication");
-    console.log("Fooditem is",item);
-    logMessage(item);
+//function foodApp(item){
+//    console.log("FoodApplication");
+  //  console.log("Fooditem is",item);
+   // logMessage(item);
+//}
+
+//function logMessage(item) {
+//    console.log("You are ordered the" ,item)
+//}
+//foodApp(foodItemName);
+
+//let deliveryLocation="Hyderabad";
+//console.log(deliveryLocation);
+
+
+//let estimationTime="20 mins";
+//console.log(estimationTime);
+
+//let isDeliveryAvailable=true;
+//console.log(isDeliveryAvailable);
+
+
+
+
+function getFullName(firstName,lastName){
+    let fullName = firstName + " " + lastName;
+
+    return fullName;
+
+}
+function greetUser(firstName,lastName){
+
+
+    let message="Hello"  + fullName + "!Welcome to javascript debugging.";
+    return message;
+
+}
+function showMessage(firstName,lastName){
+    let fullName=getFullName(firstName,lastName);
+let greeting=greetUser(fullName);
+return greeting;
 }
 
-function logMessage(item) {
-    console.log("You are ordered the" ,item)
-}
-foodApp(foodItemName);
+let finalMessage=showMessage("Sahithi","Kasa");
 
-let deliveryLocation="Hyderabad";
-console.log(deliveryLocation);
-
-
-let estimationTime="20 mins";
-console.log(estimationTime);
-
-let isDeliveryAvailable=true;
-console.log(isDeliveryAvailable);
+console.log(finalMessage);
