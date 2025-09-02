8288b6d0e641d1f0aafa1b034876007bfcff5120
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-09-02 22:26:48 +0530

    debug

---
+++ debugging-codetech.js
@@ -0,0 +1,26 @@
+let foodItemName= "Pizza";
+console.log(foodItemName);
+
+
+
+
+function foodApp(item){
+    console.log("FoodApplication");
+    console.log("Fooditem is",item);
+    logMessage(item);
+}
+
+function logMessage(item) {
+    console.log("You are ordered the" ,item)
+}
+foodApp(foodItemName);
+
+let deliveryLocation="Hyderabad";
+console.log(deliveryLocation);
+
+
+let estimationTime="20 mins";
+console.log(estimationTime);
+
+let isDeliveryAvailable=true;
+console.log(isDeliveryAvailable);
