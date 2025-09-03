e31889283d2070db7413e343e966c05038d44875
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-09-03 13:36:39 +0530

    complex

---
+++ complexdatatypes.js
@@ -0,0 +1,70 @@
+var student = {
+     sName          : "xyz",
+     dateOfbirth    : "Dec 30 , 2010",
+     subjects       : ["En" , "Hi" , "Ma"],
+     getStuLocatoin : function(){
+                          return "Banoglore"
+                      }
+
+                    }
+
+console.log("Driving");
+
+console.log(student.sName);
+
+console.log(student.getStuLocatoin());
+
+console.log  (Object.keys(student))  ;  
+
+console.log(Object.values(student));
+
+console.log(Object.entries(student));
+
+//let text="  Javascript  ";
+
+//console.log(text.length);
+
+//console.log(text.toUpperCase());
+
+//console.log(text.toLowerCase());
+//console.log(text.includes("script"));
+//console.log(text.trim());
+
+
+
+
+let Names=["Sahithi","gayatri","Akhila"];
+console.log(Names.reverse());
+let data=Names[0].trim().toLowerCase();
+console.log(data);
+
+Names.push("Anitha");
+console.log(Names);
+
+Names.pop("Anitha");
+console.log(Names);
+
+console.log(Names.indexOf("gayatri"));
+
+//firstString=" Sahithi ";
+//secondString="Gayatri";
+//     let variable=firstString.concat(secondString);
+//
+//
+//
+//
+//     console.log(variable);
+  //   var firstStringtrim=firstString.trim();
+ //    console.log(firstStringtrim);
+
+//var name=firstString.length;
+//console.log(name);
+
+
+
+
+//var string=firstString.replace("Sahithi","Akhila");
+//console.log(String);
+//console.log(firstString.slice(3,8));
+
+
