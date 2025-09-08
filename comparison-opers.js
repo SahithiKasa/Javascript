4a00203341d0dcd51610f5b57c939c82094e0922
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-09-08 22:20:27 +0530

    comparison

---
+++ comparison-opers.js
@@ -0,0 +1,38 @@
+if(1 == 100){
+    console.log("1 eqauls to 1")
+ }
+ else{
+    console.log("something not good");
+ }
+
+
+
+ 
+
+let age = 20;
+console.log(age == 20);  
+
+
+//some situaitons you need to check the value also//
+
+console.log(50 == "50")
+
+
+console.log(1  == 1);
+console.log(1  == "1");
+console.log(1  === "1") ; //please check with type also
+
+typeof(1)     // number
+typeof("1")  // string
+
+
+
+
+let studentname = "john";
+
+if(studentname == "Michal"){
+      console.log("stuent name is Michal")
+}
+else{
+    console.log("stuent name is not Michal")
+}
