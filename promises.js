30ad475f7e56e3401230c22cfd3e38d6e20a2472
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-09-11 23:18:52 +0530

    promises

---
+++ promises.js
@@ -0,0 +1,84 @@
+// resolve -- callbackfuntion
+
+// reject -- callbackfuntion
+
+
+//promise is a function which accepts the anonomous function as a input paramter
+
+//2 input paramters 
+
+//first is one indicaes the succsscallbackfunciton
+//second is indicates the failcallbakcfuntion
+
+
+// 1. function - I do't know
+
+// 2. Promise is a kind of funtion  new function
+
+
+//now you have created the promise
+
+
+//var johnCar = new Car("Dec 10 , 2020" , "John" , "UK");
+
+//Promise definaiton
+let myPromise = new Promise((resolve, reject) => {
+    let myStatus = true;
+    if (myStatus) {
+        resolve("success message");
+    }
+    else {
+        reject("fail Message");
+    }
+})
+
+
+//wneever hey then is a kind of member in promise
+
+//toppercase() in
+
+
+// catch
+
+//
+myPromise.then(
+    (successresultSet) => {
+        //  you can write our movie
+    }).catch((failMessage) => {
+        console.log(failMessage);
+    })
+
+
+
+
+
+
+
+
+    function downloadFile() {
+  return new Promise((resolve, reject) => {
+    console.log("Starting download...");
+ 
+    setTimeout(() => {
+      let success = true; // change to false to test rejection
+      if (success) {
+        resolve("⬇️ File downloaded successfully!");
+      } else {
+        reject("❌ File download failed!");
+      }
+    }, 3000); // takes 3 seconds
+  });
+}
+ 
+// Consuming the promise with .then() and .catch()
+downloadFile()
+  .then((message) => {
+    console.log("Resolved:", message);
+  })
+  .catch((error) => {
+    console.log("Rejected:", error);
+  })
+  .finally(() => {
+    console.log("Download process finished.");
+  });
+ 
--- sync call.js
+++ sync call.js
@@ -11,3 +11,31 @@ function step3(){
 step1();   // some time 
 step2();
 step3();
+
+
+
+
+
+
+
+
+// Main function that accepts a callback
+function greetUser(name, callback) {
+  console.log("Hello, " + name);
+  
+  // Calling the callback function immediately (synchronously-if the callback is synchronous it is executed immediately
+  //during the executio of the main func not later )//
+
+  callback();
+}
+ 
+// Callback function
+function sayGoodbye() {
+  console.log("Goodbye!");
+}
+ 
+// Calling the main function with a callback
+greetUser("Sahithi", sayGoodbye);
+ 
+console.log("This line runs after the callback is done.");
+ 
